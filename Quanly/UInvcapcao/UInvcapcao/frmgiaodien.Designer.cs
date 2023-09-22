ea632e2b81644b05ba668b2ee3c1d7ff2f220426
Kiến Lương Châu <luong211003@gmail.com>
2023-09-22 12:53:12 +0700

    add frm giaodien

--- Quanly/UInvcapcao/UInvcapcao/frmgiaodien.Designer.cs
+++ Quanly/UInvcapcao/UInvcapcao/frmgiaodien.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace UInvcapcao
 {
-    partial class frmgiaodien
+    partial class frmGiaoDien
     {
         /// <summary>
         /// Required designer variable.
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmgiaodien));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGiaoDien));
             this.sidebar = new System.Windows.Forms.FlowLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
@@ -78,21 +78,21 @@
             this.sidebar.Controls.Add(this.panel7);
             this.sidebar.Dock = System.Windows.Forms.DockStyle.Left;
             this.sidebar.Location = new System.Drawing.Point(0, 0);
-            this.sidebar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.sidebar.MaximumSize = new System.Drawing.Size(222, 544);
-            this.sidebar.MinimumSize = new System.Drawing.Size(74, 544);
+            this.sidebar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.sidebar.MaximumSize = new System.Drawing.Size(296, 670);
+            this.sidebar.MinimumSize = new System.Drawing.Size(99, 670);
             this.sidebar.Name = "sidebar";
-            this.sidebar.Size = new System.Drawing.Size(74, 544);
+            this.sidebar.Size = new System.Drawing.Size(99, 670);
             this.sidebar.TabIndex = 0;
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.menubuttom);
-            this.panel1.Location = new System.Drawing.Point(2, 2);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Location = new System.Drawing.Point(3, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(220, 98);
+            this.panel1.Size = new System.Drawing.Size(293, 121);
             this.panel1.TabIndex = 0;
             // 
             // label1
@@ -101,20 +101,19 @@
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(82, 43);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(109, 53);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 24);
+            this.label1.Size = new System.Drawing.Size(73, 29);
             this.label1.TabIndex = 1;
             this.label1.Text = "Menu";
             // 
             // menubuttom
             // 
             this.menubuttom.Image = global::UInvcapcao.Properties.Resources.sidebar_button5;
-            this.menubuttom.Location = new System.Drawing.Point(7, 28);
-            this.menubuttom.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.menubuttom.Location = new System.Drawing.Point(9, 34);
+            this.menubuttom.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.menubuttom.Name = "menubuttom";
-            this.menubuttom.Size = new System.Drawing.Size(48, 47);
+            this.menubuttom.Size = new System.Drawing.Size(64, 58);
             this.menubuttom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.menubuttom.TabIndex = 0;
             this.menubuttom.TabStop = false;
@@ -125,10 +124,10 @@
             this.panel2.Controls.Add(this.btnhome);
             this.panel2.Controls.Add(this.panel3);
             this.panel2.Controls.Add(this.panel4);
-            this.panel2.Location = new System.Drawing.Point(2, 104);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel2.Location = new System.Drawing.Point(3, 127);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(220, 74);
+            this.panel2.Size = new System.Drawing.Size(293, 91);
             this.panel2.TabIndex = 0;
             // 
             // btnhome
@@ -140,9 +139,9 @@
             this.btnhome.Image = ((System.Drawing.Image)(resources.GetObject("btnhome.Image")));
             this.btnhome.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnhome.Location = new System.Drawing.Point(0, 2);
-            this.btnhome.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnhome.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnhome.Name = "btnhome";
-            this.btnhome.Size = new System.Drawing.Size(218, 69);
+            this.btnhome.Size = new System.Drawing.Size(291, 85);
             this.btnhome.TabIndex = 1;
             this.btnhome.Text = "  Home";
             this.btnhome.UseVisualStyleBackColor = false;
@@ -150,28 +149,28 @@
             // 
             // panel3
             // 
-            this.panel3.Location = new System.Drawing.Point(1, 76);
-            this.panel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel3.Location = new System.Drawing.Point(1, 94);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(220, 72);
+            this.panel3.Size = new System.Drawing.Size(293, 89);
             this.panel3.TabIndex = 1;
             // 
             // panel4
             // 
-            this.panel4.Location = new System.Drawing.Point(0, 74);
-            this.panel4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel4.Location = new System.Drawing.Point(0, 91);
+            this.panel4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(220, 72);
+            this.panel4.Size = new System.Drawing.Size(293, 89);
             this.panel4.TabIndex = 1;
             this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
             // 
             // panel5
             // 
             this.panel5.Controls.Add(this.btncategory);
-            this.panel5.Location = new System.Drawing.Point(2, 182);
-            this.panel5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel5.Location = new System.Drawing.Point(3, 222);
+            this.panel5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(220, 72);
+            this.panel5.Size = new System.Drawing.Size(293, 89);
             this.panel5.TabIndex = 1;
             this.panel5.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
             // 
@@ -184,9 +183,9 @@
             this.btncategory.Image = ((System.Drawing.Image)(resources.GetObject("btncategory.Image")));
             this.btncategory.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btncategory.Location = new System.Drawing.Point(0, 0);
-            this.btncategory.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btncategory.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btncategory.Name = "btncategory";
-            this.btncategory.Size = new System.Drawing.Size(218, 69);
+            this.btncategory.Size = new System.Drawing.Size(291, 85);
             this.btncategory.TabIndex = 1;
             this.btncategory.Text = "       Category";
             this.btncategory.UseVisualStyleBackColor = false;
@@ -195,10 +194,10 @@
             // panel6
             // 
             this.panel6.Controls.Add(this.btnhelp);
-            this.panel6.Location = new System.Drawing.Point(2, 258);
-            this.panel6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel6.Location = new System.Drawing.Point(3, 315);
+            this.panel6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(220, 72);
+            this.panel6.Size = new System.Drawing.Size(293, 89);
             this.panel6.TabIndex = 1;
             this.panel6.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
             // 
@@ -211,9 +210,9 @@
             this.btnhelp.Image = ((System.Drawing.Image)(resources.GetObject("btnhelp.Image")));
             this.btnhelp.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnhelp.Location = new System.Drawing.Point(0, 0);
-            this.btnhelp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnhelp.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnhelp.Name = "btnhelp";
-            this.btnhelp.Size = new System.Drawing.Size(218, 69);
+            this.btnhelp.Size = new System.Drawing.Size(291, 85);
             this.btnhelp.TabIndex = 1;
             this.btnhelp.Text = "Help";
             this.btnhelp.UseVisualStyleBackColor = false;
@@ -221,10 +220,10 @@
             // panel7
             // 
             this.panel7.Controls.Add(this.btnabout);
-            this.panel7.Location = new System.Drawing.Point(2, 334);
-            this.panel7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel7.Location = new System.Drawing.Point(3, 408);
+            this.panel7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(220, 72);
+            this.panel7.Size = new System.Drawing.Size(293, 89);
             this.panel7.TabIndex = 1;
             this.panel7.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
             // 
@@ -237,9 +236,9 @@
             this.btnabout.Image = ((System.Drawing.Image)(resources.GetObject("btnabout.Image")));
             this.btnabout.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnabout.Location = new System.Drawing.Point(0, 2);
-            this.btnabout.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnabout.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnabout.Name = "btnabout";
-            this.btnabout.Size = new System.Drawing.Size(218, 69);
+            this.btnabout.Size = new System.Drawing.Size(291, 85);
             this.btnabout.TabIndex = 1;
             this.btnabout.Text = "  About";
             this.btnabout.UseVisualStyleBackColor = false;
@@ -253,21 +252,22 @@
             // pictureBox7
             // 
             this.pictureBox7.Image = global::UInvcapcao.Properties.Resources.fastandfurious;
-            this.pictureBox7.Location = new System.Drawing.Point(376, 280);
-            this.pictureBox7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox7.Location = new System.Drawing.Point(501, 345);
+            this.pictureBox7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(107, 128);
+            this.pictureBox7.Size = new System.Drawing.Size(143, 158);
             this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox7.TabIndex = 2;
             this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
             // 
             // pictureBox6
             // 
             this.pictureBox6.Image = global::UInvcapcao.Properties.Resources.luavsnuoc;
-            this.pictureBox6.Location = new System.Drawing.Point(553, 280);
-            this.pictureBox6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox6.Location = new System.Drawing.Point(737, 345);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(106, 128);
+            this.pictureBox6.Size = new System.Drawing.Size(141, 158);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox6.TabIndex = 2;
             this.pictureBox6.TabStop = false;
@@ -276,65 +276,69 @@
             // pictureBox5
             // 
             this.pictureBox5.Image = global::UInvcapcao.Properties.Resources.shin;
-            this.pictureBox5.Location = new System.Drawing.Point(717, 280);
-            this.pictureBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox5.Location = new System.Drawing.Point(956, 345);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(106, 128);
+            this.pictureBox5.Size = new System.Drawing.Size(141, 158);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox5.TabIndex = 2;
             this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
             // 
             // pictureBox4
             // 
             this.pictureBox4.Image = global::UInvcapcao.Properties.Resources.conan;
-            this.pictureBox4.Location = new System.Drawing.Point(717, 122);
-            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox4.Location = new System.Drawing.Point(956, 150);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(106, 128);
+            this.pictureBox4.Size = new System.Drawing.Size(141, 158);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 2;
             this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
             // 
             // pictureBox3
             // 
             this.pictureBox3.Image = global::UInvcapcao.Properties.Resources.bong_dung_trung_so;
-            this.pictureBox3.Location = new System.Drawing.Point(553, 122);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox3.Location = new System.Drawing.Point(737, 150);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(106, 128);
+            this.pictureBox3.Size = new System.Drawing.Size(141, 158);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 2;
             this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = global::UInvcapcao.Properties.Resources.avengers;
-            this.pictureBox2.Location = new System.Drawing.Point(376, 122);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox2.Location = new System.Drawing.Point(501, 150);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(107, 128);
+            this.pictureBox2.Size = new System.Drawing.Size(143, 158);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.pictureBox1.Image = global::UInvcapcao.Properties.Resources.logowithwords;
-            this.pictureBox1.Location = new System.Drawing.Point(503, 10);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(671, 12);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(155, 73);
+            this.pictureBox1.Size = new System.Drawing.Size(207, 90);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
-            // frmgiaodien
+            // frmGiaoDien1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(979, 544);
+            this.ClientSize = new System.Drawing.Size(1305, 670);
             this.Controls.Add(this.pictureBox7);
             this.Controls.Add(this.pictureBox6);
             this.Controls.Add(this.pictureBox5);
@@ -343,8 +347,8 @@
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.sidebar);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.Name = "frmgiaodien";
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "frmGiaoDien1";
             this.Text = "Giao Diện Bán Vé";
             this.sidebar.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
